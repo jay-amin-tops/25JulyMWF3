e028c427fa9c94ec28903d94dd573e25e85ea2ba
jay-amin-tops <jay.amin.tops@gmail.com>
2022-10-10 16:14:01 +0530

    js

--- PHP/MVC/views/loginpage.php
+++ PHP/MVC/views/loginpage.php
@@ -13,12 +13,14 @@
                         <form method="post">
                             <div class="row">
                                 <div class="col">
-                                    <input type="text" placeholder="Enter User Name or Email" class="form-control" name="username" id="username" required>
+                                    <input type="text" onblur="checkreq(this,'usernameerror')" placeholder="Enter User Name or Email" class="form-control" name="username" id="username" required>
+                                    <span id="usernameerror"></span>
                                 </div>
                             </div>
                             <div class="row mt-3">
                                 <div class="col">
-                                    <input type="text" placeholder="Enter Password" class="form-control" name="password" id="password">
+                                    <input type="text" onblur="checkreq(this,'passworderror')" placeholder="Enter Password" class="form-control" name="password" id="password">
+                                    <span id="passworderror"></span>
                                 </div>
                             </div>
                             <div class="row mt-3">
@@ -40,3 +42,30 @@
     </div>
 </div>
 </section>
+<script>
+    document.getElementById('username').addEventListener("keyup",function () {
+        // console.log("called");
+        // var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        var RegX = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
+        if (RegX.test(this.value)) {
+            console.log("inside if");
+            document.getElementById("usernameerror").innerHTML = ""
+        } else {
+            console.log("inside else");
+            document.getElementById("usernameerror").innerHTML = "Invalid formate"
+            
+        }
+    })
+function checkreq(e,spn) {
+    // uname = document.getElementById("username").value
+    // console.log(uname);
+    if (e.value == "") {
+        document.getElementById(spn).innerHTML = "This field is required !!!"
+        // console.log("This field is required");
+    } else {
+        document.getElementById(spn).innerHTML = ""
+        console.log("Done");
+    }
+    // console.log("called ");
+}
+</script>
